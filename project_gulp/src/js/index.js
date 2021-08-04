dee35c467a07689a3299a594e28c71dd31bdf230
Kateryna <ms.ant1997@gmail.com>
2021-08-04 20:44:08 +0200

    homework 17 added to github

--- project_gulp/src/js/index.js
+++ project_gulp/src/js/index.js
@@ -1,4 +1,15 @@
-import Shady from './components/shady';
 
-const slimShady = new Shady('Slim');
-console.log(slimShady.fullName);
+  $(document).ready(function(){
+   function initSlider() {
+       console.log('.slider');
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'  
+    });
+   }
+   initSlider();
+  });
